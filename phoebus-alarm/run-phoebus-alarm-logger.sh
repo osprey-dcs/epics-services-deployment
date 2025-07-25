201103a02f210debc0cf4a17899179b850ace534
Diego Omitto <domitto@users.noreply.github.com>
2025-07-25 16:08:45 -0400

    Update run-phoebus-alarm-logger.sh
    Change from -SNAPSHOT to tag

--- phoebus-alarm/run-phoebus-alarm-logger.sh
+++ phoebus-alarm/run-phoebus-alarm-logger.sh
@@ -5,7 +5,7 @@ export PATH="$JAVA_HOME/bin:$PATH"
 
 # figure out the path to the product jar
 if [[ -z "${PHOEBUS_ALARM_LOGGER_JAR}" ]]; then
-  PHOEBUS_ALARM_LOGGER_JAR=service-alarm-logger-4.7.4-SNAPSHOT.jar
+  PHOEBUS_ALARM_LOGGER_JAR=service-alarm-logger-4.7.4.jar
 fi
 
 JDK_JAVA_OPTIONS=" -DCA_DISABLE_REPEATER=true"
